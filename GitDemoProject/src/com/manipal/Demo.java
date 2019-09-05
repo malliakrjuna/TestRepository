cbf9c8b9e9cb25cfa96f2f450edc764d838268fa
ITFS <ITFS@ITFS-17>
2019-09-05 11:26:32 +0530

    Saving after adding 2nd line Save it in remote repo

--- GitDemoProject/src/com/manipal/Demo.java
+++ GitDemoProject/src/com/manipal/Demo.java
@@ -3,7 +3,8 @@ package com.manipal;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("hello git");
+		System.out.println("hello git....");
+		System.out.println("hiiii git....");
 
 	}
 
